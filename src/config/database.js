84c822a0c89b6d849258223d555086de07a6e947
Leandro Mello <leandro.mello@qualita.inf.br>
2019-10-23 14:00:55 -0300

    First Commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  url: "postgres://pbjxxuuy:9XvjZSXHHBjfOl158najI6KPaopypCTr@tuffi.db.elephantsql.com:5432/pbjxxuuy",
+  host: "tuffi.db.elephantsql.com",
+  username: "pbjxxuuy",
+  password: "9XvjZSXHHBjfOl158najI6KPaopypCTr",
+  database: "pb", 
+  define: {
+    timestamps: true,
+
+  },
+};
