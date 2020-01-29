cea14da22dd75ca15cdd929c39104a985fc8b034
Leandro Mello <leandro.mello@qualita.inf.br>
2020-01-29 10:52:13 -0300

    add heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,23 @@
-module.exports = {
-  dialect: 'postgres',
-  //url: "postgres://pbjxxuuy:9XvjZSXHHBjfOl158najI6KPaopypCTr@tuffi.db.elephantsql.com:5432/pbjxxuuy",
-  host: 'rajje.db.elephantsql.com',
-  username: 'qzlqoacr',
-  password: 'j_3CgHFoMfk8ewaUMUEZZtS0xeVgfD5X',
-  database: 'qzlqoacr', 
-  define: {
-    timestamps: true,
-    underscored: false,
+var dbConfig = {};
+if (process.env.DATABASE_URL){
+  dbConfig = {
+    dialect : 'postgres',
+    protocol: 'postgres',
+  }
+}else{
+  dbConfig = {
+    dialect: 'postgres',
+    //url: "postgres://pbjxxuuy:9XvjZSXHHBjfOl158najI6KPaopypCTr@tuffi.db.elephantsql.com:5432/pbjxxuuy",
+    host: 'rajje.db.elephantsql.com',
+    username: 'qzlqoacr',
+    password: 'j_3CgHFoMfk8ewaUMUEZZtS0xeVgfD5X',
+    database: 'qzlqoacr', 
+    define: {
+      timestamps: true,
+      underscored: false,
 
-  },
-};
+    },
+  }
+}
+
+module.exports = dbConfig;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 const autoReap = require('multer-autoreap');
-
+const PORT = process.env.PORT || 3333;
 require('./database');
 
 app.use(express.json());
@@ -11,4 +11,4 @@ app.use(cors());
 app.use(autoReap);
 
 app.use(routes);
-app.listen(3333);
+app.listen(PORT);
