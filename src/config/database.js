482b24b6fc09d4925a08337ef5f4a4befd225543
Leandro Mello <leandro.mello@qualita.inf.br>
2020-01-29 10:56:49 -0300

    changing database

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,13 @@
-var dbConfig = {};
-if (process.env.DATABASE_URL){
-  dbConfig = {
-    dialect : 'postgres',
-    protocol: 'postgres',
-  }
-}else{
-  dbConfig = {
-    dialect: 'postgres',
-    //url: "postgres://pbjxxuuy:9XvjZSXHHBjfOl158najI6KPaopypCTr@tuffi.db.elephantsql.com:5432/pbjxxuuy",
-    host: 'rajje.db.elephantsql.com',
-    username: 'qzlqoacr',
-    password: 'j_3CgHFoMfk8ewaUMUEZZtS0xeVgfD5X',
-    database: 'qzlqoacr', 
-    define: {
-      timestamps: true,
-      underscored: false,
+module.exports = {
+  dialect: 'postgres',
+  //url: "postgres://pbjxxuuy:9XvjZSXHHBjfOl158najI6KPaopypCTr@tuffi.db.elephantsql.com:5432/pbjxxuuy",
+  host: 'rajje.db.elephantsql.com',
+  username: 'qzlqoacr',
+  password: 'j_3CgHFoMfk8ewaUMUEZZtS0xeVgfD5X',
+  database: 'qzlqoacr', 
+  define: {
+    timestamps: true,
+    underscored: false,
 
-    },
-  }
-}
-
-module.exports = dbConfig;
+  },
+};
