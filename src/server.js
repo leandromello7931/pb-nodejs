9de16db723b21d297ee018e543de15c5e660f27f
Leandro Mello <leandro.mello@qualita.inf.br>
2020-01-31 10:07:38 -0300

    improving cors

--- src/server.js
+++ src/server.js
@@ -5,13 +5,22 @@ const cors = require('cors');
 const autoReap = require('multer-autoreap');
 const dotenv = require('dotenv-safe');
 const PORT = process.env.PORT || 3333;
-
-
-
 require('./database');
+
+const whitelist = ['http:/localhost:3333', 'https://backend-pb.herokuapp.com/'];
+const corsOptions = {
+  origin: function(origin, callback){
+    if (whitelist.indexOf(origin) !== -1){
+      callback(null, true);
+    }else{
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(autoReap);
 
 app.use(routes);
