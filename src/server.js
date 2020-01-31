e159e67b5c89b03386651400c2bb072ff90dd3e4
Leandro Mello <leandro.mello@qualita.inf.br>
2020-01-31 10:55:48 -0300

    improving cors

--- src/server.js
+++ src/server.js
@@ -22,6 +22,8 @@ dotenv.config();
 
 app.use((req, res) => {
   req.header("Access-Control-Allow-Origin", "*");
+  req.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  req.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
