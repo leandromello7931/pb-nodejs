dc5dd5eef137b01e111105fba0538bbb5034831d
Leandro Mello <leandro.mello@qualita.inf.br>
2020-01-31 14:18:52 -0300

    fixing user route

--- src/server.js
+++ src/server.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
-const app = express();
+
 const cors = require('cors');
 const autoReap = require('multer-autoreap');
 const dotenv = require('dotenv-safe');
 const PORT = process.env.PORT || 3333;
 require('./database');
 
-const whitelist = ['http:/localhost:3333', 'https://backend-pb.herokuapp.com/', 'http://localhost:3000'];
-const corsOptions = {
-  origin: function(origin, callback){
-    if (whitelist.indexOf(origin) !== -1){
-      callback(null, true);
-    }else{
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}
-
+const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors(corsOptions));
-app.options('*', cors());
-
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // Set to true if you need the website to include cookies in the requests sent
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
-
-
+app.use(cors());
+dotenv.config();
 
 app.use(autoReap);
 
